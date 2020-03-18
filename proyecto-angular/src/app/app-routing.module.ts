9c656b42f324bc48baff7acf71b83b30de46b0dd
Pedro <castro.fcefyn@gmail.com>
2020-03-18 20:46:35 -0300

    Creacion de componentes y Routing. OK

--- proyecto-angular/src/app/app-routing.module.ts
+++ proyecto-angular/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
+//Angular
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Components
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'sobre-mi', component: AboutComponent},
+  {path: 'proyectos', component: ProjectsComponent},
+  {path: 'crear-proyecto', component: CreateComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
